88007eac587eb025db2d70c65f1bbc8e24a65ebd
farhikhta farzan <farhikhtafarzan@gmail.com>
2024-08-05 16:35:28 -0400

    package modification

--- farzanPortfolio/src/components/AboutMe.jsx
+++ farzanPortfolio/src/components/AboutMe.jsx
@@ -7,10 +7,11 @@ function AboutMe() {
         <p className={style.aboutMeDesc}>
           {" "}
           Hello! I am Farhikhta Farzan. I am currently pursuing my Bachelor’s
-          degree in computer science. I am interested in software development.
-          My experiences include software development and product management. I
-          have experience working with C++, Java, Express.js, React, MongoDB,
-          MySQL, and Python. If you want to learn more about me, feel free to dm
+          degree in computer science. My journey so far has been a mix of
+          software development and product management, with hands-on experience
+          in C++, Java, Express.js, React, MongoDB, MySQL, and Python. When I am
+          not coding, I am probably diving into a good book. If you would like to
+          connect, discuss tech, or share book recommendations, feel free to DM
           me on LinkedIn!
         </p>
       </div>
