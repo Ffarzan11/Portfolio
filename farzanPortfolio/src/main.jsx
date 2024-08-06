f7fceb7c9d5791a76097fc9bbce2f3d6a8c90a5d
farhikhta farzan <farhikhtafarzan@gmail.com>
2024-08-05 20:58:59 -0400

    removed route

--- farzanPortfolio/src/main.jsx
+++ farzanPortfolio/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename={"/http://Ffarzan11.github.io/portfolio/"}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
--- farzanPortfolio/vite.config.js
+++ farzanPortfolio/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/http://Ffarzan11.github.io/portfolio/",
   server: {
     port: 3000
   }
