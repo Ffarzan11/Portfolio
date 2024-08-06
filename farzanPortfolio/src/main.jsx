a8791b6ca1efaf5e60496a47ad104f5637734b6f
farhikhta farzan <farhikhtafarzan@gmail.com>
2024-08-05 20:48:15 -0400

    router fixed

--- farzanPortfolio/src/main.jsx
+++ farzanPortfolio/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <Router basename={"/http://Ffarzan11.github.io/portfolio/"}>
       <App />
     </Router>
   </React.StrictMode>
--- farzanPortfolio/vite.config.js
+++ farzanPortfolio/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/http://Ffarzan11.github.io/portfolio/",
   server: {
     port: 3000
   }
